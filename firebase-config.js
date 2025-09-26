cc715d6858afbb21ff5ce99d677f4b4efb5e1e75
smpnegeri10banawaselatan-dev <smpnegeri10banawaselatan@gmail.com>
2025-09-26 19:12:40 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhq_cDM8crJGkIq2m7YGz1YTAJ5l6T7jU",
+  authDomain: "absensisiswa-ce885.firebaseapp.com",
+  projectId: "absensisiswa-ce885",
+  storageBucket: "absensisiswa-ce885.firebasestorage.app",
+  messagingSenderId: "839652887403",
+  appId: "1:839652887403:web:2d72804567b61e33041a2e",
+  measurementId: "G-3TJG23K099"
+};
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
